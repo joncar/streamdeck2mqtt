890ddb6263be424947480b1b2ee8f1fbfe0370a9
Jon Caruana <jon@joncaruana.com>
2021-11-29 00:00:04 +0000

    Code cleanup.

--- streamdeck2mqtt.py
+++ streamdeck2mqtt.py
@@ -3,6 +3,7 @@ import threading
 import logging
 import re
 import json
+import signal
 
 from PIL import Image, ImageDraw, ImageFont
 from StreamDeck.DeviceManager import DeviceManager
@@ -10,129 +11,108 @@ from StreamDeck.ImageHelpers import PILHelper
 
 import paho.mqtt.client as mqtt
 
-def render_key_image(deck_sn, deck, icon_text, label_text):
-    global icon_fonts_by_sn
-    global label_font
-    image = PILHelper.create_image(deck)
+class StreamDeck2MQTT:
+    def render_key_image(self, icon_text, label_text):
+        image = PILHelper.create_image(self.deck)
 
-    draw = ImageDraw.Draw(image)
+        draw = ImageDraw.Draw(image)
 
-    if icon_text:
-        icon_font = icon_fonts_by_sn[deck_sn]
-        v = (image.height - 20) if label_text else image.height
-        draw.text((image.width / 2, v / 2), text=icon_text, font=icon_font, anchor="mm", fill="white")
+        if icon_text:
+            v = (image.height - 20) if label_text else image.height
+            draw.text((image.width / 2, v / 2), text=icon_text, font=self.icon_font, anchor="mm", fill="white")
 
-    if label_text:
-        v = (image.height - 5) if icon_text else (image.height / 2)
-        draw.text((image.width / 2, v), text=label_text, font=label_font, anchor="ms", fill="white")
+        if label_text:
+            v = (image.height - 5) if icon_text else (image.height / 2)
+            draw.text((image.width / 2, v), text=label_text, font=self.label_font, anchor="ms", fill="white")
 
-    return PILHelper.to_native_format(deck, image)
+        return PILHelper.to_native_format(self.deck, image)
 
-def on_connect(client, userdata, flags, rc):
-    global client_id
-    global streamdecks
-    global serial_numbers
+    def on_connect(self, client, userdata, flags, rc):
+        self.client.subscribe(f"streamdeck/{self.deck_sn}/#")
 
-    print("MQTT: Connected with result code "+str(rc))
+        self.client.publish(f'streamdeck/{self.deck_sn}/availability', 'online', retain=True)
 
-    client.subscribe("streamdeck/#")
-
-    client.publish(f'streamdeck/{client_id}/availability', 'online', retain=True)
-
-    for index, deck in enumerate(streamdecks):
-        deck_sn = serial_numbers[deck.id()]
-        for key_id in range(deck.key_count()):
+        for key_id in range(self.deck.key_count()):
             config = json.dumps({
-                "unique_id": f"streamdeck_{deck_sn}_{key_id}",
+                "unique_id": f"streamdeck_{self.deck_sn}_{key_id}",
                 "name": f"StreamDeck Key {key_id}", 
-                "state_topic": f"streamdeck/{deck_sn}/{key_id}/state",
-                "availability_topic": f"streamdeck/{client_id}/availability",
-                "json_attributes_topic": f"streamdeck/{deck_sn}/{key_id}/attributes",
+                "state_topic": f"streamdeck/{self.deck_sn}/{key_id}/state",
+                "availability_topic": f"streamdeck/{self.deck_sn}/availability",
+                "json_attributes_topic": f"streamdeck/{self.deck_sn}/{key_id}/attributes",
                 "icon": "mdi:keyboard",
                 "device": {
-                    "identifiers": [deck_sn],
+                    "identifiers": [self.deck_sn],
                     "name": "StreamDeck"
                 }
             })
-            client.publish(f'homeassistant/binary_sensor/streamdeck_{deck_sn}_{key_id}/config', config, retain=True)
-            client.publish(f"streamdeck/{deck_sn}/{key_id}/attributes", json.dumps({
+            self.client.publish(f'homeassistant/binary_sensor/streamdeck_{self.deck_sn}_{key_id}/config', config, retain=True)
+            self.client.publish(f"streamdeck/{self.deck_sn}/{key_id}/attributes", json.dumps({
                 "number": key_id
             }), retain=True)
-            
+                
 
-def on_message(client, userdata, msg):
-    global streamdecks_by_sn
-    global keys
+    def on_message(self, client, userdata, msg):
+        p = re.compile(r'streamdeck/([^/]+)/(\d+)/(text|icon|set)')
+        m = p.match(msg.topic)
+        if m:
+            deck_sn = m.group(1)
+            key_id = int(m.group(2))
+            prop = m.group(3)
+            value = msg.payload.decode('utf-8')
 
-    print(f'MQTT: {msg.topic} = {str(msg.payload)}')
+            key = self.keys.setdefault(key_id, {})
 
-    p = re.compile(r'streamdeck/([^/]+)/(\d+)/(text|icon|set)')
-    m = p.match(msg.topic)
-    if m:
-        deck_sn = m.group(1)
-        key_id = int(m.group(2))
-        prop = m.group(3)
-        value = msg.payload.decode('utf-8')
-        print(f'RE: Deck="{deck_sn}" Key="{key_id}" Prop="{prop}" = "{value}"')
+            if prop == 'set':
+                self.keys[key_id] = key = json.loads(value)
+            else:
+                key[prop] = value
 
-        deck = streamdecks_by_sn[deck_sn]
-        key = keys.setdefault(key_id, {})
+            image = self.render_key_image(key.get('icon'), key.get('text'))
+            with self.deck:
+                self.deck.set_key_image(key_id, image)
 
-        if prop == 'set':
-            keys[key_id] = key = json.loads(value)
-        else:
-            key[prop] = value
+    def on_key_change(self, deck, key, state):
+        self.client.publish(f'streamdeck/{self.deck_sn}/{key}/state', 'ON' if state else 'OFF', retain=True)
 
-        image = render_key_image(deck_sn, deck, key.get('icon'), key.get('text'))
-        with deck:
-            deck.set_key_image(key_id, image)
+    def __init__(self, deck):
+        self.deck = deck
+        self.keys = {}
 
-def on_key_change(deck, key, state):
-    global client
-    global serial_numbers
-    print(f'StreamDeck: Deck {serial_numbers[deck.id()]} Key {key} = {state}', flush=True)
+    def start(self, config):
+        self.deck.open()
+        self.deck.reset()
 
-    client.publish(f'streamdeck/{serial_numbers[deck.id()]}/{key}/state', 'ON' if state else 'OFF', retain=True)
+        self.deck.set_brightness(30)
+        self.deck.set_key_callback(self.on_key_change)
+
+        self.deck_sn = self.deck.get_serial_number().replace('\0', '').replace('\x01', '')
+
+        key_width, key_height = self.deck.key_image_format()['size']
+        self.icon_font = ImageFont.truetype('materialdesignicons-webfont.ttf', key_height)
+
+        self.label_font = ImageFont.truetype('Roboto-Regular.ttf', 14)
+
+        client_id = f"streamdeck2mqtt_{self.deck_sn}"
+        self.client = mqtt.Client(client_id=client_id, clean_session=False)
+        self.client.username_pw_set(config['mqtt_username'], config['mqtt_password'])
+        self.client.will_set(f'streamdeck/{self.deck_sn}/availability', 'offline')
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        if config.get('debug'):
+            self.client.enable_logger()
+
+        self.client.connect(config['mqtt_server'])
+        self.client.loop_start()
 
 with open("config.json") as json_data_file:
     config = json.load(json_data_file)
-print(config)
 
 if config.get('debug'):
     logging.basicConfig(level=logging.DEBUG)
 
-label_font = ImageFont.truetype('Roboto-Regular.ttf', 14)
+for deck in DeviceManager().enumerate():
+    worker = StreamDeck2MQTT(deck)
+    worker.start(config)
 
-streamdecks = DeviceManager().enumerate()
-serial_numbers = {}
-streamdecks_by_sn = {}
-icon_fonts_by_sn = {}
-keys = {}
-
-print(f'Found {len(streamdecks)} Stream Deck(s)')
-
-for index, deck in enumerate(streamdecks):
-    deck.open()
-    deck.reset()
-
-    deck.set_brightness(30)
-    deck.set_key_callback(on_key_change)
-
-    sn = deck.get_serial_number().replace('\0', '').replace('\x01', '')
-    serial_numbers[deck.id()] = sn
-    streamdecks_by_sn[sn] = deck
-
-    key_width, key_height = deck.key_image_format()['size']
-
-    icon_fonts_by_sn[sn] = ImageFont.truetype('materialdesignicons-webfont.ttf', key_height)
-
-client_id = "streamdeck2mqtt"
-client = mqtt.Client(client_id=client_id, clean_session=False)
-client.username_pw_set(config['mqtt_username'], config['mqtt_password'])
-client.will_set(f'streamdeck/{client_id}/availability', 'offline')
-client.on_connect = on_connect
-client.on_message = on_message
-client.enable_logger()
-
-client.connect(config['mqtt_server'])
-client.loop_forever()
+signal.pause()
